e4be5269610433d4ef734f827311da1f0283c232
Calvin Richards <calvinrrichards64@gmail.com>
2026-04-23 12:28:19 -0700

    began modifying friction values

--- Assets/Scripts/CurlingStone.cs
+++ Assets/Scripts/CurlingStone.cs
@@ -6,12 +6,33 @@ public class CurlingStone : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-
+    private Collider baseCollider;
+    private Collider bodyCollider;
     private float desired_acceleration_x;
     private float desired_acceleration_y;
 
     void Start()
     {
+
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+
+        foreach (Collider col in colliders) {
+            if (col.gameObject.name == "CurlingStone_Col_Base") {
+                baseCollider = col;
+            }
+            else if (col.gameObject.name == "CurlingStone_Col_Body") {
+                bodyCollider = col;
+            }
+        }
+
+        baseCollider.material = new PhysicsMaterial("BaseMaterial");
+        bodyCollider.material = new PhysicsMaterial("BodyMaterial");
+        
+        bodyCollider.material.dynamicFriction = 0.3f; // change at some point
+        bodyCollider.material.staticFriction = 0.3f;
+        
+        // probably add some base value for the friction here        
+
         desired_acceleration_x = 0;
         desired_acceleration_y = 0;    
     }
@@ -35,6 +56,11 @@ public class CurlingStone : MonoBehaviour
         var movement = action.Get<Vector2>();
         desired_acceleration_x = movement.x;
         desired_acceleration_y = movement.y;
+    }
+    
+    public void SetIceFriction(float f) {
+        baseCollider.material.dynamicFriction = f; // should obviously be changed later
+        baseCollider.material.staticFriction = f;
     }
 
 }
